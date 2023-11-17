edba429684e579a41da43677237b99978eccb451
blacktunes <blacktunes@foxmail.com>
2023-11-17 15:42:33 +0800

    feat: 完善消息组件基础样式

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="message">
     <div class="message-box">
-      <div class="image">
+      <div class="left">
         <img
           src="@/assets/empty.webp"
           alt=""
         />
       </div>
-      <div class="content"></div>
+      <div class="right">
+        <div class="user">
+          <div class="avatar"></div>
+          <div class="name">洞天风水讲座</div>
+        </div>
+        <div class="content">
+          <div class="title">绥园这么整是要抽干罗浮仙舟的气运啊！</div>
+          <div class="text">
+            <span class="text_highlight">#见鬼实录#</span
+            >吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦
+          </div>
+          <div class="read-all">阅读全文</div>
+          <div class="line"></div>
+          <div class="comment-num">共10条评论</div>
+          <div class="comment-list">
+            <div class="comment">
+              <div class="user">
+                <div class="avatar"></div>
+                <div class="name">洞天风水讲座</div>
+              </div>
+              <div class="text">
+                啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦
+              </div>
+              <div class="reply-list">
+                <div class="reply">
+                  <div class="user">
+                    <div class="avatar"></div>
+                    <div class="name">洞天风水讲座</div>
+                  </div>
+                  <div class="text">
+                    啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦
+                  </div>
+                </div>
+              </div>
+              <div class="comment-line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +55,24 @@ console.log(1)
 </script>
 
 <style lang="stylus" scoped>
+@import '../assets/style.styl'
+
+.user
+  display flex
+  align-items center
+
+  .avatar
+    width 40px
+    height 40px
+    border-radius 50%
+    background #666
+    margin-right 10px
+
+  .name
+    font-size 18px
+    color var(--name-color)
+    margin-bottom 3px
+
 .message
   position absolute
   top 0
@@ -45,26 +101,101 @@ console.log(1)
       height 100%
       background-image url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='69.283' height='40' patternTransform='scale(2) rotate(30)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M46.189-20L57.736 0M46.189 20l11.547 20m-46.189 0l11.547 20M11.547 0l11.547 20m40.415 30H40.415M28.868 30H5.774m23.094-40H5.774m57.735 20H40.415m0 20L28.868 50m11.547-60L28.868 10m46.188 0L63.509 30M5.774 10L-5.773 30m75.056 10H46.189L34.64 20 46.19 0h23.094C73.13 6.667 76.98 13.333 80.83 20zM57.736 60H34.64L23.094 40l11.547-20h23.095c3.848 6.667 7.698 13.333 11.547 20L57.736 60zm0-40H34.64L23.094 0l11.547-20h23.095L69.283 0c-3.87 6.7-8.118 14.06-11.547 20zM34.64 60H11.547L0 40l11.547-20h23.094L46.19 40 34.64 60zm0-40H11.547L0 0l11.547-20h23.094L46.19 0 34.64 20zM23.094 40H0l-5.773-10-5.774-10L0 0h23.094l11.547 20-11.547 20z'  stroke-width='1' stroke='hsla(197, 40%, 14%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")
 
-    .image
+    .left
       z-index 5
       width 770px
       height 770px
 
-    .content
+    .right
+      overflow hidden
+      display flex
+      flex-direction column
       z-index 5
       position relative
       flex 1
+      padding 30px 10px 20px 35px
       margin-right 15px
-      background radial-gradient(farthest-corner at 40% 45%, #23252e 0%, rgba(35, 37, 46, 0.9) 75%, rgba(38, 58, 69, 0.7) 100%)
 
-      &:after
+      &:before
+        z-index -2
         content ''
         position absolute
+        top 50%
+        left 50%
+        width 930px
+        height 930px
+        background radial-gradient(rgba(35, 37, 46, 0.9) 10%, rgba(35, 37, 46, 0.9) 50%, rgba(38, 58, 69, 0.7)), radial-gradient(rgba(35, 37, 46, 0.9), rgba(38, 58, 69, 0))
+        background-size 130% 130%, 80%, 80%
+        background-position -50% -50%, 50% 50%
+        animation 10s rotate linear infinite
+
+      &:after
+        z-index -1
+        content ''
+        position absolute
+        top 0
+        left 0
         width 100%
         height 100%
         background url('@/assets/icon.webp')
         background-repeat no-repeat
         background-position center
         background-size 320px
-        opacity 0.1
+        opacity 0.05
+
+      .content
+        flex 1
+        overflow auto
+        margin-top 25px
+        padding-right 20px
+
+        .name
+          name()
+
+        .title
+          title()
+
+        .text
+          margin-top 20px
+          text()
+
+        .read-all
+          color #42a8b9
+          text-align right
+          font-size 20px
+
+        .line
+          width 100%
+          height 2px
+          background #3e454d
+          margin-top 20px
+
+        .comment-num
+          text()
+          margin 20px 0
+
+        .comment-list
+          .comment
+            .name
+              color #59666e
+
+            .text
+              margin-left 50px
+              margin-top 0
+
+            .reply-list
+              margin 15px 0 0 50px
+
+            .comment-line
+              width calc(100% - 50px)
+              height 2px
+              background #3e454d
+              margin 15px 0 15px 50px
+
+@keyframes rotate
+  from
+    transform translate(-50%, -50%) rotate(0deg)
+
+  to
+    transform translate(-50%, -50%) rotate(360deg)
 </style>
