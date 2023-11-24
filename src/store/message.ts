0507466f7ed44436203c4cca29771741ab4351fb
blacktunes <blacktunes@foxmail.com>
2023-11-24 17:15:00 +0800

    feat: 增加数据持久化

--- src/store/message.ts
+++ src/store/message.ts
@@ -1,69 +1,40 @@
 import { reactive } from 'vue'
+import avatar from '@/assets/avatar/1.webp'
 
 export const message = reactive<{
   list: Message[]
 }>({
   list: [
     {
-      id: 100,
+      id: 1700816535616,
       user: {
         id: 1,
-        avatar:
-          'https://patchwiki.biligame.com/images/sr/thumb/9/93/n6bsmrgi3as9gttm6av8zxb3f90r5bb.png/180px-%E4%BD%A9%E4%BD%A9%EF%BC%88%E5%A4%B4%E5%83%8F%EF%BC%89.png',
-        name: '里生外熟'
-      },
-      is_like: false,
-      like: 0,
-      title: '随便一个标题',
-      text: '#鬼话连篇##鬼话连篇1##鬼话连篇2##鬼话连篇3##鬼话连篇4##鬼话连篇5##鬼话连篇6##鬼话连篇7#吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧  啦吧啦吧啦吧啦吧啦',
-      time: 0,
-      comments: []
-    },
-    {
-      id: 200,
-      user: {
-        id: 1,
-        avatar:
-          'https://patchwiki.biligame.com/images/sr/thumb/9/93/n6bsmrgi3as9gttm6av8zxb3f90r5bb.png/180px-%E4%BD%A9%E4%BD%A9%EF%BC%88%E5%A4%B4%E5%83%8F%EF%BC%89.png',
-        name: '里生外熟'
+        name: '小桂子Guinevere',
+        avatar: avatar
       },
       is_like: true,
-      like: 99999,
-      image:
-        'https://patchwiki.biligame.com/images/sr/thumb/1/14/d2v9gsa9uwv9y9l4co9vv3cbuv1icf7.png/1200px-%E6%9D%A5%E8%87%AA%E5%A4%A7%E4%BC%9F%E5%93%A5%E7%9A%84%E9%80%9A%E8%AE%AF%EF%BC%9A%E3%80%8C%E6%98%9F%E6%B5%B7%E4%B9%8B%E6%97%85%EF%BC%8C%E6%84%9F%E8%B0%A2%E5%90%8C%E8%A1%8C%E3%80%8D.png.jpeg',
-      title: '有没有人在长乐天遇到一个说要比棋的怪人？',
-      text: '@123 #见鬼实录# #鬼话连篇#最近在长乐天冒出了一个怪人，吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧  啦吧啦吧啦吧啦吧啦\n吧啦吧啦<span class="text_highlight">123</span>吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦',
-      time: 2,
+      like: 9998,
+      title: '这是简单的使用说明',
+      text: '#使用说明#双井号括起来的会自动识别为话题\n@咸鱼 at符号加名字后面跟个空格会识别为at\n点击下面的修改按钮可以修改内容\n左边有个按钮可以增加/修改图片\n点赞数可以直接修改',
+      time: 1700816865344,
       comments: [
         {
           user: {
             id: 1,
-            avatar:
-              'https://patchwiki.biligame.com/images/sr/thumb/9/93/n6bsmrgi3as9gttm6av8zxb3f90r5bb.png/180px-%E4%BD%A9%E4%BD%A9%EF%BC%88%E5%A4%B4%E5%83%8F%EF%BC%89.png',
-            name: '里生外熟'
+            name: '小桂子Guinevere',
+            avatar: avatar
           },
-          text: '吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦',
+          text: '点击上面的评论按钮可以发送评论\n点击评论会弹出菜单',
           comments: [
             {
               user: {
                 id: 1,
-                avatar:
-                  'https://patchwiki.biligame.com/images/sr/thumb/9/93/n6bsmrgi3as9gttm6av8zxb3f90r5bb.png/180px-%E4%BD%A9%E4%BD%A9%EF%BC%88%E5%A4%B4%E5%83%8F%EF%BC%89.png',
-                name: '里生外熟'
+                name: '小桂子Guinevere',
+                avatar: avatar
               },
-              text: '吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦'
+              text: '没了，其他功能随缘更新'
             }
           ]
-        },
-        {
-          user: {
-            id: 1,
-            avatar:
-              'https://patchwiki.biligame.com/images/sr/thumb/9/93/n6bsmrgi3as9gttm6av8zxb3f90r5bb.png/180px-%E4%BD%A9%E4%BD%A9%EF%BC%88%E5%A4%B4%E5%83%8F%EF%BC%89.png',
-            name: '里生外熟'
-          },
-          text: '吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦',
-          comments: []
         }
       ]
     }
